e82035c51f4ebae08ab009ee8e3787ee51f724c8
Marcin Mejner <marczu09@gmail.com>
2018-04-17 15:29:35 +0200

    commit 1

---
+++ app/src/main/java/com/marcinmejner/czatownik/login/CreateUserActivity.kt
@@ -0,0 +1,13 @@
+package com.marcinmejner.czatownik.login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.marcinmejner.czatownik.R
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+}
---
+++ app/src/main/java/com/marcinmejner/czatownik/login/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.marcinmejner.czatownik.login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.marcinmejner.czatownik.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginCreateNewUserBtnClicked(view: View){
+
+
+    }
+
+    fun loginLoginBtnClicked(view: View){
+
+    }
+}
