eab381c20eb1ba4764ad8f77fde0c281cc8d2e37
Marcin Mejner <marczu09@gmail.com>
2018-04-24 12:12:10 +0200

    commit 11 dodatnie socketio dla sciagania wiadomości z kanałów

---
+++ app/src/main/java/com/marcinmejner/czatownik/Services/MessageService.kt
@@ -0,0 +1,60 @@
+package com.marcinmejner.czatownik.Services
+
+import android.content.Context
+import android.util.Log
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import com.marcinmejner.czatownik.Controller.App
+import com.marcinmejner.czatownik.Model.Channel
+import com.marcinmejner.czatownik.Model.Message
+import com.marcinmejner.czatownik.Utils.URL_GET_CHANNELS
+import org.json.JSONException
+
+object MessageService {
+    private val TAG = "MessageService"
+
+    val channels = ArrayList<Channel>()
+    val messages = ArrayList<Message>()
+
+    fun getChannels(context: Context, complete: (Boolean) -> Unit) {
+
+
+        val channelRequest = object : JsonArrayRequest(Method.GET, URL_GET_CHANNELS, null, Response.Listener { response ->
+
+            channels.clear()
+
+            try {
+                for (x in 0 until response.length()) {
+                    val channel = response.getJSONObject(x)
+                    val name = channel.getString("name")
+                    val channelDesc = channel.getString("description")
+                    val channelId = channel.getString("_id")
+
+                    val newChannel = Channel(name, channelDesc, channelId)
+                    this.channels.add(newChannel)
+                }
+                complete(true)
+
+            } catch (e: JSONException) {
+                Log.d(TAG, "getChannels: ${e.localizedMessage}")
+            }
+
+        }, Response.ErrorListener { error ->
+            Log.d(TAG, "getChannels: Could not retrive channels")
+            complete(false)
+        }) {
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers.put("Authorization", "Bearer ${App.prefs.authToken}")
+                return headers
+            }
+        }
+        App.prefs.requestQueue.add(channelRequest)
+    }
+
+}
